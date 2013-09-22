4b5ec828a758b25a532666257784259f1c9dcfbc
Jonathan Stark <jstark@jonathanstark.com>
2013-09-22 02:47:28 -0400

    Created FastActive file

---
+++ FastActive.js
@@ -0,0 +1,17 @@
+(function (d, c) {
+    var activeClass = c || "active";
+    function clearActive() {
+        var actives = d.querySelectorAll("." + activeClass);
+        for (var i = 0; i < actives.length; i++) {
+            actives[i].classList.remove(activeClass);
+        }
+    }
+    function setActive(e) {
+        clearActive();
+        if (e.target.tagName == "A") {
+            e.target.classList.add(activeClass);
+        }
+    }
+    d.body.addEventListener("touchstart", setActive, false);
+    d.body.addEventListener("touchmove" , clearActive, false);
+})(document, "active");
