412bb518099540cb678727a5e2d8832d034189f5
Jonathan Stark <jstark@jonathanstark.com>
2013-09-27 16:53:06 -0400

    removed onmousedown event

--- FastActive.js
+++ FastActive.js
@@ -14,5 +14,4 @@
     }
     d.body.addEventListener("mousedown" , setActive, false);
     d.body.addEventListener("touchstart", setActive, false);
-    d.body.addEventListener("touchmove" , clearActive, false);
 })(document, "active");
