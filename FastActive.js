b8e6b7c55b0230b06e81e13ac8738a20f1cf9f1d
Jonathan Stark <jstark@jonathanstark.com>
2013-09-28 20:24:17 -0400

    added touch/no-touch test and refactored a bit

--- FastActive.js
+++ FastActive.js
@@ -1,19 +1,24 @@
 (function (d, activeClass) {
-    var activeLink = false;
     function clearActive() {
-        if (activeLink) {
-            activeLink.classList.remove(activeClass);
-            activeLink = false;
+        if (activeElement) {
+            activeElement.classList.remove(activeClass);
+            activeElement = false;
         }
     }
     function setActive(e) {
         clearActive();
-        if (e.target.tagName == "A") {
-            activeLink = e.target;
+        if (e.target.tagName == 'A') {
+            activeElement = e.target;
             e.target.classList.add(activeClass);
         }
     }
-    d.body.addEventListener("mousedown", setActive, false);
-    d.body.addEventListener("touchstart", setActive, false);
-    d.body.addEventListener("touchmove", clearActive, false);
-})(document, "active");
+    var activeElement = false;
+    if(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+        document.documentElement.classList.add('touch');
+    } else {
+        document.documentElement.classList.add('no-touch');
+    }
+    d.body.addEventListener('mousedown', setActive, false);
+    d.body.addEventListener('touchstart', setActive, false);
+    d.body.addEventListener('touchmove', clearActive, false);
+})(document, 'active');
